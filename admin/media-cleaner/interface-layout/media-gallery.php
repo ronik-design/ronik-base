466388c375b8dc03bdb7303192bc66889827f980
KmancusoRonik <105605229+KmancusoRonik@users.noreply.github.com>
2024-03-20 13:57:24 -0400

    Rework logic

--- admin/media-cleaner/interface-layout/media-gallery.php
+++ admin/media-cleaner/interface-layout/media-gallery.php
@@ -75,27 +75,38 @@ function rmc_media_column_value( $column_name, $id ) {
 add_action( 'manage_media_custom_column', 'rmc_media_column_value', 10, 2 );
 
 
-function media_add_author_dropdown(){
+function mediacleaner_add_dropdown(){
     $scr = get_current_screen();
     if ( $scr->base !== 'upload' ) return;
 
     $rbp_media_cleaner_filter  = isset($_GET['rbp_media_cleaner_filter']) ? $_GET['rbp_media_cleaner_filter'] : '';
-    $selected_1 = $rbp_media_cleaner_filter == 'rbp_media_cleaner_isdetached_true' ? 'selected="selected"' : '';
-    $selected_2 = $rbp_media_cleaner_filter == 'rbp_media_cleaner_isdetached_false' ? 'selected="selected"' : '';
-    $selected_3 = $rbp_media_cleaner_filter == 'rbp_media_cleaner_isdetached_preserved' ? 'selected="selected"' : '';
+    $isdetached_selected_1 = $rbp_media_cleaner_filter == 'rbp_media_cleaner_isdetached_true' ? 'selected="selected"' : '';
+    $isdetached_selected_2 = $rbp_media_cleaner_filter == 'rbp_media_cleaner_isdetached_false' ? 'selected="selected"' : '';
+    $isdetached_selected_3 = $rbp_media_cleaner_filter == 'rbp_media_cleaner_isdetached_preserved' ? 'selected="selected"' : '';
+
+    $rbp_media_cleaner_size_filter  = isset($_GET['rbp_media_cleaner_size_filter']) ? $_GET['rbp_media_cleaner_size_filter'] : '';
+    $isdetached_selected_1 = $rbp_media_cleaner_size_filter == 'rbp_media_cleaner_size_large' ? 'selected="selected"' : '';
+    $isdetached_selected_2 = $rbp_media_cleaner_size_filter == 'rbp_media_cleaner_size_small' ? 'selected="selected"' : '';
+
     echo '
         <select name="rbp_media_cleaner_filter" id="rbp_media_cleaner_filter" class="">
             <option value="-1">Is Media Safe to Delete?</option>
-            <option value="rbp_media_cleaner_isdetached_true" '.$selected_1.'>Safe to Delete</option>
-            <option value="rbp_media_cleaner_isdetached_false" '.$selected_2.'>Not Safe to Delete</option>
-            <option value="rbp_media_cleaner_isdetached_preserved" '.$selected_3.'>Preserved Images</option>
+            <option value="rbp_media_cleaner_isdetached_true" '.$isdetached_selected_1.'>Safe to Delete</option>
+            <option value="rbp_media_cleaner_isdetached_false" '.$isdetached_selected_2.'>Not Safe to Delete</option>
+            <option value="rbp_media_cleaner_isdetached_preserved" '.$isdetached_selected_3.'>Preserved Images</option>
+        </select>
 
+        <select name="rbp_media_cleaner_size_filter" id="rbp_media_cleaner_size_filter" class="">
+            <option value="-1">File Size</option>
+            <option value="rbp_media_cleaner_size_large" '.$isdetached_selected_1.'>File Size Large</option>
+            <option value="rbp_media_cleaner_size_small" '.$isdetached_selected_2.'>File Size Small</option>
         </select>
     ';
 }
-add_action('restrict_manage_posts', 'media_add_author_dropdown');
+add_action('restrict_manage_posts', 'mediacleaner_add_dropdown');
 
-function author_filter($query) {
+
+function mediacleaner_filter($query) {
     if ( is_admin() && $query->is_main_query() ) {
         if (!isset($_GET['rbp_media_cleaner_filter'])){
             $query->set('rbp_media_cleaner_filter', '');
@@ -122,7 +133,6 @@ function author_filter($query) {
             if ($_GET['rbp_media_cleaner_filter'] == 'rbp_media_cleaner_isdetached_false') {
                 $meta_query[] = array(
                     'relation'  => 'AND',
-
                     array (
                         'key'     => '_wp_attachment_metadata',
                         'value'   => 'rbp_media_cleaner_isdetached_temp-saved',
@@ -133,15 +143,9 @@ function author_filter($query) {
                         'value'   => 'rbp_media_cleaner_isdetached_true',
                         'compare' => 'NOT LIKE'
                     ),
-
-
-                    // 'key' => '_wp_attachment_metadata',
-                    // 'value' => 'rbp_media_cleaner_isdetached_true' ,
-                    // 'compare' => 'NOT LIKE', // works!
                 );
             }
         }
-
         if ( isset($_GET['rbp_media_cleaner_filter']) ) {
             if ($_GET['rbp_media_cleaner_filter'] == 'rbp_media_cleaner_isdetached_preserved') {
                 $meta_query[] = array(
@@ -151,8 +155,99 @@ function author_filter($query) {
                 );
             }
         }
+
+
+
+
+
+        if (!isset($_GET['rbp_media_cleaner_size_filter'])){
+            $query->set('rbp_media_cleaner_size_filter', '');
+        }
+        $meta_query[] = array();
+        // Reset filter.
+        if ( isset($_GET['rbp_media_cleaner_size_filter']) ) {
+            if($_GET['rbp_media_cleaner_size_filter'] == -1){
+                $query->set('rbp_media_cleaner_size_filter', '');
+            }
+        }
+        // Reset filter.
+        if ( isset($_GET['rbp_media_cleaner_size_filter']) ) {
+            if ($_GET['rbp_media_cleaner_size_filter'] == 'rbp_media_cleaner_size_large') {
+                $mishaValue = $_GET['rbp_media_cleaner_size_filter'];
+                // $meta_query[] = array(
+                //     'key'     => 'filesize',
+                //     // 'value'   => $mishaValue,
+                //     // 'compare' => 'LIKE'
+                // );
+
+                // $query->set('order', 'asc');
+                // $query->set('orderby', '_wp_attachment_image_filesize');
+
+
+
+                // $meta_query[] = array(
+                //     'relation' => 'OR',
+                //     array(
+                //         'key' => '_wp_attachment_image_filesize',
+                //         'compare' => 'NOT EXISTS',
+                //     ),
+                //     array(
+                //         'key' => '_wp_attachment_image_filesize',
+                //     ),
+                // );
+
+                $orderby = $query->get( 'orderby' );
+
+                if ( 'filesize' == $orderby ) {
+            
+                    $meta_query = array(
+                        'relation' => 'OR',
+                        array(
+                            'key' => '_wp_attachment_image_filesize',
+                            'compare' => 'NOT EXISTS',
+                        ),
+                        array(
+                            'key' => '_wp_attachment_image_filesize',
+                        ),
+                    );
+            
+                    $query->set( 'meta_query', $meta_query );
+                    $query->set( 'orderby', 'meta_value' );
+                }
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
         // Set the meta query to the complete, altered query
         $query->set('meta_query',$meta_query);
     }
 }
-add_action('pre_get_posts','author_filter');
+add_action('pre_get_posts','mediacleaner_filter');
+
+
+/* ------------------------------ MEDIA METADATA ------------------------------ */
+// edits image meta data
+add_action( 'add_attachment', 'my_edit_image_meta_data' );
+function my_edit_image_meta_data( $post_ID ) {
+    // checks if the uploaded file is an image
+    if ( wp_attachment_is_image( $post_ID ) ) {
+        // finds the total file / image size
+        $filesize = filesize( get_attached_file( $post_ID ) );
+        // converts bits to mega bytes
+        $filesize_convert = $filesize / 1024 / 1024;
+        // converts number to format based on locale
+        $filesize  = number_format_i18n( $filesize_convert, 3 );
+        // creates new meta field with file size of an image
+        update_post_meta( $post_ID, '_wp_attachment_image_filesize', $filesize );
+    }
+}
